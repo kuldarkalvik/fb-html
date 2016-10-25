4b174273ebd4dd2ee54d2c3815b87503409bed50
Kuldar Kalvik <hi@kuldar.com>
2016-10-25 16:23:40 +0300

    Add some UI styles

---
+++ public/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+	$('.header-open').on('click', function() {
+		$('.sidebar').addClass('is-open');
+		$('.header-open').addClass('is-hidden');
+	});
+
+	$('.sidebar-close').on('click', function() {
+		$('.sidebar').removeClass('is-open');
+		$('.header-open').removeClass('is-hidden');
+	});
+
+	$('.sidebar-nav-group-title').on('click', function() {
+		$('.sidebar-nav-group').removeClass('is-active');
+		$('.sidebar-nav-group').children('.sidebar-nav-group-items').slideUp(300, 'easeInOutQuad');
+
+		$(this).parent('.sidebar-nav-group').addClass('is-active');
+		$(this).siblings('.sidebar-nav-group-items').slideDown(300, 'easeInOutQuad');
+
+	})
+
+});
