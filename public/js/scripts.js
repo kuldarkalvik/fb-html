614db54f2b0279833a5f9b99241d3dbc4f3f7045
Kuldar Kalvik <hi@kuldar.com>
2016-10-27 10:29:55 +0300

    Stuff

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,22 +1,52 @@
 $(document).ready(function(){
 
-	$('.header-open').on('click', function() {
+	// Open sidebar
+	$('.header-open').on('click', function(e) {
+		e.preventDefault();
+
 		$('.sidebar').addClass('is-open');
 		$('.header-open').addClass('is-hidden');
+		$('.container').removeClass('is-full');
 	});
 
-	$('.sidebar-close').on('click', function() {
+	// Close sidebar
+	$('.sidebar-close').on('click', function(e) {
+		e.preventDefault();
+
 		$('.sidebar').removeClass('is-open');
 		$('.header-open').removeClass('is-hidden');
+		$('.container').addClass('is-full');
 	});
 
-	$('.sidebar-nav-group-title').on('click', function() {
+	// Open sidebar navigation group
+	$('.sidebar-nav-group-title').on('click', function(e) {
+		e.preventDefault();
+
 		$('.sidebar-nav-group').removeClass('is-active');
 		$('.sidebar-nav-group').children('.sidebar-nav-group-items').slideUp(200, 'easeInOutQuad');
 
 		$(this).parent('.sidebar-nav-group').addClass('is-active');
 		$(this).siblings('.sidebar-nav-group-items').slideDown(200, 'easeInOutQuad');
+	});
 
-	})
+	// Toggle sidebar user nav
+	$('.sidebar-user').on('click', function(e) {
+		e.preventDefault();
+		$('.sidebar-user-nav').toggleClass('is-open').slideToggle(200, 'easeInOutQuad');
+	});
+
+	// Sticky the page nav
+	var elementOffset = $('.page-nav').offset().top;
+
+	$(document).on('scroll', function() {
+		var scrollTop = $(window).scrollTop();
+
+		if (scrollTop > elementOffset) {
+			$('.page-nav').addClass('is-fixed');
+		} else {
+			$('.page-nav').removeClass('is-fixed');
+		}
+
+	});
 
 });
